93099025c2440af984a1cbe92b5a518023850f6e
MHMAsuk <mhmasuk.personal@gmail.com>
2022-08-23 18:12:33 +0600

    Add students pages.

---
+++ components/Students/StudentList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StudentList = () => {
+    return (
+        <>
+            <h1 className="font-weight-bold">Student List</h1>
+        </>
+    )
+}
+
+export default StudentList;
---
+++ pages/students.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import StudentList from "../components/Students/StudentList";
+
+const Students = () => {
+    return (
+        <>
+            <StudentList />
+        </>
+    )
+}
+
+export default StudentList;
