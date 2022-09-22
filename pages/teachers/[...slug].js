e2a054374393e1b67f509be38708071c5a198a1a
jibon969 <jayed.swe@gmail.com>
2022-09-22 21:06:37 +0600

    added teacherfetch api

--- pages/teachers/[...slug].js
+++ pages/teachers/[...slug].js
@@ -1,14 +1,20 @@
 import React from "react";
+import {useRouter} from 'next/router'
+
 
 // TeacherDetails
 import TeacherDetails from "../../components/Teachers/TeacherDetails";
+import {getTeacherDetailApi, getAllTeacher} from "../api/TeacherAPI/teacher_api";
 
 const TeacherDetail = () => {
+
     return (
         <>
-            <TeacherDetails/>
+            <TeacherDetails />
         </>
     )
 };
+
+
 
 export default TeacherDetail;
