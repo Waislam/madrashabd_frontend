58fea81bccbb56fe2377355017ba9f21ae3a886d
Waislam <waislam67@gmail.com>
2022-10-07 17:21:59 +0600

    talimat issue solved

--- pages/talimat/examination.js
+++ pages/talimat/examination.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 // Examination Component
-import Examination from "../../components/Talimat/Examination";
+// import Examination from "../../components/Talimat/Examination";
+import Examination from "../../components/Talimat/Examination/Examination"
 import Layout from "../../components/Layout/Layout";
 
 const ExaminationPage = () => {
