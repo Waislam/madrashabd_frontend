cf5c80466f946111b81cdf129d2b359a682f6514
jayed969 <jayed.swe@gmail.com>
2022-10-29 14:38:24 +0600

    added boarding fetch & post data

--- pages/boarding/add-bazar.js
+++ pages/boarding/add-bazar.js
@@ -5,33 +5,41 @@ import Layout from "../../components/Layout/Layout";
 const AddBazarPage = () => {
 
     const [madrasha, setMadrasha] = useState('');
-    const [date, setDate] = useState('');
     const [bazar_item_name, setBazarItemName] = useState('');
     const [quantity, setQuantity] = useState('');
+    const [measurement, setMeasurement] = useState('');
     const [amount, setAmount] = useState('');
     const [consumption, setConsumption] = useState('');
-    const [isPending, setPending] = useState(false);
+    const [date, setDate] = useState('');
 
-    const onClickFormHandler = () => {
-        let data = {madrasha, date, bazar_item_name, quantity, amount, consumption};
-        setPending(true);
-        fetch("http://127.0.0.1:8000/boarding/bazarlist/", {
+    const handleBazarListForm = (event) => {
+        event.preventDefault();
+        fetch("http://127.0.0.1:8086/boarding/bazarlist/100/", {
             method: "POST",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data)
-        }).then((resp) => {
-            resp.json().then(() => {
-                setDate('');
-                setMadrasha('');
-                setBazarItemName('');
-                setQuantity('');
-                setConsumption('');
-                setPending(false);
-            })
-        })
+            body: JSON.stringify(
+                {
+                    "madrasha": 1,
+                    "item": {
+                        "madrasha": 1,
+                        "bazar_item_name": bazar_item_name,
+                        "quantity": quantity,
+                        "measurement": measurement,
+                        "amount": amount,
+                        "consumption": consumption,
+                    },
+                    "date": date
+                },
+            )
+        }).then((res) => res.json())
+            .catch((err) => {
+                console.log(err.message)
+            });
+
+
     };
 
     return (
@@ -43,15 +51,14 @@ const AddBazarPage = () => {
                             <div className="card-body">
                                 <h3>Add New Bazar</h3>
                                 <hr/>
-                                <form action="#" method="POST">
+                                <form method="POST">
                                     <div className="row">
                                         <div className="col-md-4 mb-3">
                                             <input
-                                                type="text"
-                                                placeholder="Date"
+                                                type="number"
+                                                placeholder="Madrasha"
                                                 className="form-control"
                                                 name="madrasha"
-                                                value={madrasha}
                                                 onChange={(e) => {
                                                     setMadrasha(e.target.value)
                                                 }}
@@ -59,7 +66,7 @@ const AddBazarPage = () => {
                                         </div>
                                         <div className="col-md-4 mb-3">
                                             <input
-                                                type="text"
+                                                type="date"
                                                 placeholder="Date"
                                                 className="form-control"
                                                 name="date"
@@ -108,6 +115,15 @@ const AddBazarPage = () => {
                                             />
                                         </div>
                                         <div className="col-md-4 mb-3">
+                                            <select className="form-select" name="measurement" onChange={(e) => {
+                                                setMeasurement(e.target.value)
+                                            }}>
+                                                <option value="kg">Kg</option>
+                                                <option value="litre">Litre</option>
+                                                <option value="gm">Gm</option>
+                                            </select>
+                                        </div>
+                                        <div className="col-md-4 mb-3">
                                             <input
                                                 type="text"
                                                 placeholder="Consumption"
@@ -121,11 +137,13 @@ const AddBazarPage = () => {
                                             />
                                         </div>
                                     </div>
-                                    {!isPending &&
-                                    <button type="button" className="btn btn-primary" onClick={onClickFormHandler}>Add
-                                        Contact</button>}
-                                    {isPending &&
-                                    <button type="button" className="btn btn-primary" disabled>Adding .......</button>}
+
+
+                                    <button
+                                        className="btn btn-primary"
+                                        onClick={handleBazarListForm}>
+                                        Submit
+                                    </button>
                                 </form>
                             </div>
                         </div>
