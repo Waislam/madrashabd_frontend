6d264755654c1d26154206b107c66e9fbb836f30
jayed969 <jayed.swe@gmail.com>
2022-11-02 22:02:33 +0600

    addd transport

--- pages/transport/index.js
+++ pages/transport/index.js
@@ -1,16 +1,71 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-// Transport Componenet
+// Transport Component
 import Transport from "../../components/Transport/Transport"
 import Layout from "../../components/Layout/Layout";
+import AddTransportModal from "../../components/Transport/Modals/AddTransportModal"
+import api from "../api/api";
 
-const TransportPage = () =>{
-    return(
+
+const TransportPage = () => {
+
+    const [isLoading, setLoading] = useState(null);
+
+    // Add
+    const [transport, setTransport] = useState(null);
+    const [addTransportModal, setTransportModal] = useState(false);
+
+
+    // Get Transport
+    const getTransport = async () => {
+        const list = await api.get("http://127.0.0.1:8086/transport/100/transport-list/");
+        const data = list.data;
+        setTransport(data);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getTransport()
+    }, []);
+
+
+    // Add TransportModal
+    const handleAddTransportModal = () => {
+        setTransportModal(true)
+    };
+
+
+    if (isLoading) {
+        return (
+            <div className="text-center">
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        )
+    }
+
+    if (!transport) {
+        return (
+            <h2 className="text-center">No data found</h2>
+        )
+    }
+
+    return (
         <>
-            <Transport/>
+            <Transport
+                transport={transport}
+                handleAddTransportModal={handleAddTransportModal}
+            />
+
+            <AddTransportModal
+                show={addTransportModal}
+                onHide={() => setTransportModal(false)}
+            />
+
         </>
     )
-}
+};
 
 export default TransportPage;
 
@@ -20,4 +75,4 @@ TransportPage.getLayout = (page) => {
             {page}
         </Layout>
     )
-}
+};
