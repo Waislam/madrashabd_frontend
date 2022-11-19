dc7c2e93a65741c32af777f4705a29afcbe68fb5
jayed969 <jayed.swe@gmail.com>
2022-11-19 14:08:01 +0600

    added building fetch & get api

--- pages/setting/building.js
+++ pages/setting/building.js
@@ -1,16 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { getSession, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import React, {useEffect, useState} from "react";
+import {getSession, useSession} from "next-auth/react";
+import {useRouter} from "next/router";
 
 
 import Building from '../../components/Setting/Building'
+import AddBuildingModal from "../../components/Setting/Modals/AddBuildingModal"
+import UpdateBuildingModal from "../../components/Setting/Modals/UpdateBuildingModal"
 import Layout from "../../components/Layout/Layout";
 import api from "../api/api";
 
 const BuildingPage = (props) => {
 
     const router = useRouter();
-    const { data: session, status } = useSession();
+    const {data: session, status} = useSession();
+
+    // BuildingModal
+    const [isLoading, setLoading] = useState(null);
+    const [addBuildingModal, setBuildingModal] = useState(false);
+    const [updateBuildingModal, setUpdateBuildingModal] = useState(false);
+    const [buildingOldData, setBuildingOldData] = useState(null);
 
     useEffect(() => {
         if (status === "unauthenticated") {
@@ -19,18 +27,52 @@ const BuildingPage = (props) => {
     });
 
 
+    // Add Building
+    const handleBuildingModal = () => {
+        setBuildingModal(true)
+    };
+
+
+    // update Building
+    const handleUpdateBuildingModal = async (id) => {
+        setLoading(true);
+        const list = await api.get(`settings/building/detail/${id}/`);
+        const data = list.data;
+        setBuildingOldData(data);
+        setLoading(false);
+        setUpdateBuildingModal(true)
+    };
+
+
     return (
         <>
             <Building
                 building={props.building_list}
+                handleBuildingModal={handleBuildingModal}
+                handleUpdateBuildingModal={handleUpdateBuildingModal}
             />
+
+            <AddBuildingModal
+                session_data={props.session_data}
+                show={addBuildingModal}
+                onHide={() => setBuildingModal(false)}
+            />
+
+
+            {isLoading ? " " :
+                <UpdateBuildingModal
+                    show={updateBuildingModal}
+                    onHide={() => setUpdateBuildingModal(false)}
+                    building_old_data={buildingOldData}
+                />
+            }
         </>
     )
 };
 
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps({req}) {
 
-    const session_data = await getSession({ req });
+    const session_data = await getSession({req});
     const res = await api.get(`/settings/${session_data.user?.madrasha_slug}/building/`);
     const building_list = res.data;
 
